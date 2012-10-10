7f2d92f94df45d8f0a03144f67d2b095f864a1ad
franzholz <franzholz@6a152407-d83c-435e-9478-febfe02dd9d6>
2012-10-10 11:32:03 +0000

    version 0.2.5 from TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "table".
 #
-# Auto generated 26-09-2012 21:39
+# Auto generated 10-10-2012 13:29
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'This containts a base class which you can use to make your extensions independant from any specific table. And it can be used to make multiple language support with a separate language overlay table.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '0.2.4',
+	'version' => '0.2.5',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"25a6";s:10:"README.txt";s:4:"ee2d";s:16:"ext_autoload.php";s:4:"16d7";s:12:"ext_icon.gif";s:4:"b71f";s:17:"ext_localconf.php";s:4:"8886";s:25:"lib/class.tx_table_db.php";s:4:"c4ed";s:32:"lib/class.tx_table_db_access.php";s:4:"1538";s:30:"lib/class.tx_table_db_base.php";s:4:"a724";s:26:"lib/class.tx_table_div.php";s:4:"da5d";}',
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"6d09";s:10:"README.txt";s:4:"ee2d";s:16:"ext_autoload.php";s:4:"16d7";s:12:"ext_icon.gif";s:4:"b71f";s:17:"ext_localconf.php";s:4:"8886";s:25:"lib/class.tx_table_db.php";s:4:"3059";s:32:"lib/class.tx_table_db_access.php";s:4:"1538";s:30:"lib/class.tx_table_db_base.php";s:4:"a724";s:26:"lib/class.tx_table_div.php";s:4:"da5d";}',
 	'suggests' => array(
 	),
 );
