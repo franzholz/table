24d6f3051225a361005576a2a35fd3fea6783e72
franzholz <franzholz@6a152407-d83c-435e-9478-febfe02dd9d6>
2012-12-04 17:10:43 +0000

    version 0.2.7 from TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,21 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "table".
-#
-# Auto generated 13-10-2012 22:09
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "table".
+ *
+ * Auto generated 04-12-2012 18:08
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Table Library',
 	'description' => 'This containts a base class which you can use to make your extensions independant from any specific table. And it can be used to make multiple language support with a separate language overlay table.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '0.2.6',
+	'version' => '0.2.7',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -35,14 +35,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-5.4.99',
-			'typo3' => '3.5.0-4.7.99',
+			'typo3' => '4.5.0-6.0.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"ab98";s:10:"README.txt";s:4:"ee2d";s:16:"ext_autoload.php";s:4:"16d7";s:12:"ext_icon.gif";s:4:"b71f";s:17:"ext_localconf.php";s:4:"8886";s:25:"lib/class.tx_table_db.php";s:4:"9ccb";s:32:"lib/class.tx_table_db_access.php";s:4:"1538";s:30:"lib/class.tx_table_db_base.php";s:4:"a724";s:26:"lib/class.tx_table_div.php";s:4:"da5d";}',
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"b4f1";s:16:"ext_autoload.php";s:4:"16d7";s:12:"ext_icon.gif";s:4:"b71f";s:17:"ext_localconf.php";s:4:"8886";s:10:"README.txt";s:4:"ee2d";s:25:"lib/class.tx_table_db.php";s:4:"ebcf";s:32:"lib/class.tx_table_db_access.php";s:4:"1538";s:30:"lib/class.tx_table_db_base.php";s:4:"a724";s:26:"lib/class.tx_table_div.php";s:4:"da5d";}',
 	'suggests' => array(
 	),
 );
