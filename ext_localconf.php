ed7def4e609622330e37b9abfde06040e87a45ac
franzholz <franzholz@6a152407-d83c-435e-9478-febfe02dd9d6>
2015-06-13 17:34:48 +0000

    * ready for TYPO3 7.2 * remove the constants TABLE_EXTkey and PATH_BE_table because there is the autoload file ext_autoload.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,14 +1,4 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-if (!defined ('TABLE_EXTkey')) {
-	define('TABLE_EXTkey',$_EXTKEY);
-}
-
-if (!defined ('PATH_BE_table')) {
-	define('PATH_BE_table', t3lib_extMgm::extPath(TABLE_EXTkey));
-}
-
-
-
 ?>
