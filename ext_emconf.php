f1ff7d9b4e4b7cd48b7bca06b82862a9470e42d1
franzholz <franzholz@6a152407-d83c-435e-9478-febfe02dd9d6>
2015-07-24 19:02:09 +0000

    works under TYPO3 7.4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'This containts a base class which you can use to make your extensions independant from any specific table. And it can be used to make multiple language support with a separate language overlay table.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '0.4.0',
+	'version' => '0.4.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-5.6.99',
-			'typo3' => '4.5.0-7.2.99',
+			'typo3' => '4.5.0-7.4.99',
 			'div2007' => '1.1.5-0.0.0',
 		),
 		'conflicts' => array(
