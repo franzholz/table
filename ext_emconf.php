b566e5982cdece0ab444be4256c632724005fa06
Franz Holzinger <holzinger@ttproducts.de>
2017-03-28 20:06:37 +0200

    * ready for TYPO3 8.x * add composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-7.99.99',
-			'typo3' => '4.5.0-7.99.99',
+			'typo3' => '4.5.0-8.99.99',
 			'div2007' => '1.1.5-0.0.0',
 		),
 		'conflicts' => array(
