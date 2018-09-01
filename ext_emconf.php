40593d41eafba0e4ece7861cb42082c8b6540cba
Franz Holzinger <holzinger@ttproducts.de>
2018-09-01 17:04:56 +0200

    * ready for TYPO3 9.3.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,10 @@
 
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'Table Library',
-    'description' => 'This containts a base class which you can use to make your extensions independant from any specific table. And it can be used to make multiple language support with a separate language overlay table.',
+    'description' => 'This contains a base class which you can use to make your extensions independant from any specific table. And it can be used to make multiple language support with a separate language overlay table.',
     'category' => 'misc',
     'shy' => 0,
-    'version' => '0.7.0',
+    'version' => '0.7.1',
     'dependencies' => '',
     'conflicts' => '',
     'priority' => '',
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'php' => '5.5.0-7.99.99',
-            'typo3' => '6.1.0-8.99.99',
+            'typo3' => '6.2.0-9.3.99',
             'div2007' => '1.10.1-0.0.0',
         ),
         'conflicts' => array(
