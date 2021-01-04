ceaa46baa0ecd7b7f045dd48c8da5be38c5c0f0b
Franz Holzinger <holzinger@ttproducts.de>
2021-01-04 22:02:42 +0100

    * add support for TYPO3 10.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = array(
     'title' => 'Table Library',
     'description' => 'This contains a base class which you can use to make your extensions independant from any specific table. And it can be used to make multiple language support with a separate language overlay table.',
     'category' => 'misc',
-    'version' => '0.7.8',
+    'version' => '0.8.0',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'php' => '5.5.0-7.99.99',
-            'typo3' => '6.2.0-9.5.99',
+            'typo3' => '8.7.0-10.4.99',
             'div2007' => '1.10.26-0.0.0',
         ),
         'conflicts' => array(
