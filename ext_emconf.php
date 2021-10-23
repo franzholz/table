94494e87cae5f983f17cca2ecfe9bbcdf402805d
Franz Holzinger <franz.holzinger@festwein.de>
2021-10-23 15:29:41 +0200

    restructuring

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@
  ***************************************************************/
 
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Table Library',
     'description' => 'This contains a base class which you can use to make your extensions independant from any specific table. And it can be used to make multiple language support with a separate language overlay table.',
     'category' => 'misc',
@@ -17,17 +17,17 @@ $EM_CONF[$_EXTKEY] = array(
     'author' => 'Franz Holzinger',
     'author_email' => 'franz@ttproducts.de',
     'author_company' => 'jambage.com',
-    'constraints' => array(
-        'depends' => array(
+    'constraints' => [
+        'depends' => [
             'php' => '5.5.0-7.99.99',
             'typo3' => '8.7.0-10.4.99',
             'div2007' => '1.10.26-0.0.0',
-        ),
-        'conflicts' => array(
-        ),
-        'suggests' => array(
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
             'typo3db_legacy' => '1.0.0-1.1.99',
-        ),
-    ),
-);
+        ],
+    ],
+];
 
